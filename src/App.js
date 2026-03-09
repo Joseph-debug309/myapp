690c295a617e58cf64a69142ec8425575834abe8
Joseph-debug309 <joemainabz@gmail.com>
2026-03-09 11:27:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,24 +7,45 @@ import Thirdcomponent from './components/Thirdcomponent';
 import Fourthcomponent from './components/Fourthcomponent';
 import Fifthcomponent from './components/Fifthcomponent';
 import Sixthcomponent from './components/Sixthcomponent';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Notfound from './components/Notfound';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+
+      <nav>
+        <Link to="/First" classname="navlinks">First</Link>
+        <Link to="/Second" classname="navlinks">Second</Link>
+        <Link to="/Third" classname="navlinks">Third</Link>
+        <Link to="/Fourth" classname="navlinks">Fourth</Link>
+        <Link to="/Fifth" classname="navlinks">Fifth</Link>
+        <Link to="/Sixth" classname="navlinks">Sixth</Link>
+        <Link to="/Help" classname="navlinks">Help</Link>
+        <Link to="/ContactUs" classname="navlinks">ContactUs</Link>
+      </nav>
+
       <header className="App-header">
         <h1>Welcome to Learning Reactjs</h1>
         <h2>React is fun</h2>
       </header>
       {/*Below we render/display our first component*/}
-      <Firstcomponent></Firstcomponent>
-      <Secondcomponent></Secondcomponent>
-      <Contactus></Contactus>
-      <Help></Help>
-      <Thirdcomponent></Thirdcomponent>
-      <Fourthcomponent></Fourthcomponent>
-      <Fifthcomponent/>
-      <Sixthcomponent/>
+      <Routes>
+        <Route path="/First" element={<Firstcomponent/>} />
+        <Route path="/Second" element={<Secondcomponent/>} />
+        <Route path="/Third" element={<Thirdcomponent/>} />
+        <Route path="/Fourth" element={<Fourthcomponent/>} />
+        <Route path="/Fifth" element={<Fifthcomponent/>} />
+        <Route path="/Sixth" element={<Sixthcomponent/>} />
+        <Route path="/Help" element={<Help/>} />
+        <Route path="/ContactUs" element={<Contactus/>} />
+        <Route path="*" element={<Notfound/>} />        
+      </Routes>
+      
     </div>
+    </Router>
   )
 }
 
--- src/components/Sixthcomponent.jsx
+++ src/components/Sixthcomponent.jsx
@@ -3,14 +3,15 @@ import { useState } from "react"
 const Sixthcomponent = () => {
 
     const [weight, setWeight] = useState(50)
+    const [message, setMessage] = useState("The current weight is: ")
 
     return(
         <div className="Sixthcomponent">
             <h1>Below is the sixth Component</h1>
 
-            <h4>Current weight is {weight}</h4>
+            <h4>{message}{weight}</h4>
 
-            <button onClick={ () => setWeight(55)}>Update weight</button>
+            <button onClick={ () => [setWeight(55), setMessage("The new weight is: ")]}>Update weight</button>
         </div>
     )
 }
